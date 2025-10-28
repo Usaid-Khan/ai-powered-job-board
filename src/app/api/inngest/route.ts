e6635c85990fb830b2ba77c6b6ffeff824d7702f
Usaid-Khan <usaid1521@gmail.com>
2025-10-29 01:00:20 +0500

    implement inngest setup

---
+++ src/app/api/inngest/route.ts
@@ -0,0 +1,10 @@
+import { inngest } from "@/src/services/inngest/client";
+import { serve } from "inngest/next";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+
+  ],
+});
