f3e493f75bdc5e449e8e5c14d8745aca52104c44
Usaid-Khan <usaid1521@gmail.com>
2025-10-28 23:04:59 +0500

    implement auth buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import Image from "next/image";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebarClient } from "./_AppSidebarClient";
+import Link from "next/link";
+import { LogInIcon } from "lucide-react";
+import { SignedIn, SignedOut } from "../services/clerk/components/SignInStatus";
+import { SidebarUserButton } from "../features/users/components/SidebarUserButton";
 
 export default function Home() {
   return <SidebarProvider className="overflow-y-hidden">
@@ -10,14 +13,31 @@ export default function Home() {
           <SidebarTrigger />
           <span className="text-xl text-nowrap">Go Hire</span>
         </SidebarHeader>
-        <SidebarContent></SidebarContent>
-        <SidebarFooter>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton>sdfsdf</SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarFooter>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarMenu>
+              <SignedOut>
+                <SidebarMenuItem>
+                  <SidebarMenuButton asChild>
+                    <Link href="/sign-in">
+                      <LogInIcon />
+                      <span>Log In</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SignedOut>
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+        <SignedIn>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarUserButton />
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </SignedIn>
       </Sidebar>
       <main className="flex-1">aljsdlfjasoj</main>
     </AppSidebarClient>
